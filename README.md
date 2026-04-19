da52a9930c93ea1371b9c2cd91a68d0f0a6229e4
yogeshgurav-dev <yogeshkgurav@gmail.com>
2026-04-19 15:36:35 +0530

    remove class files

--- README.md
+++
@@ -1,2 +0,0 @@
-# java-practice
-Core Java &amp; OOP practice programs
--- first java project/src/com/physicswallah/core/Demo.java
+++
@@ -1,9 +0,0 @@
-package com.physicswallah.core;
-
-public class Demo {
-
-	public static void main(String[] args) {
-		System.out.println("First Java Program");
-	}
-
-}
